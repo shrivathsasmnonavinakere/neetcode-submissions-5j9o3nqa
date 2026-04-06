76a51e10fff519666e15d3777ed8cccdcaf85e7a
shrivathsasmnonavinakere <shrivathsasmnonavinakere@gmail.com>
2026-04-06 16:11:38 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i=0,j=1;
+        int max1=0;
+        while(j<prices.size()){
+            if((prices[j]-prices[i])<0){
+                i=j;
+                j++;
+            }
+            else{
+                int z=prices[j]-prices[i];
+                max1=max(max1,z);
+                j++;
+            }
+
+        }
+        //if(max1<0){
+        //    return 0;
+        //}
+
+        return max1;
+    }
+};
+ 
+
