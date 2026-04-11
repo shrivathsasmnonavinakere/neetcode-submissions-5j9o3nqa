b798f9af91371388b2a1657c44e0238c0977b5c3
shrivathsasmnonavinakere <shrivathsasmnonavinakere@gmail.com>
2026-04-11 17:33:36 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int>hash(256,0);
+        int r=0,l=0,count=0,sindex=-1,minlen=1000000;
+        for(int i=0;i<t.size();i++){
+            hash[t[i]]++;
+        }
+        while(r<s.size()){
+            if(hash[s[r]]>0) count++;
+            hash[s[r]]--;
+            while(count==t.size()){
+                if(r-l+1<minlen){
+                    minlen=r-l+1;
+                    sindex=l;
+                }
+                hash[s[l]]++;
+                if(hash[s[l]]>0) count--;
+                    l++;
+            }
+            r++;
+
+        }
+        return sindex==-1?"":s.substr(sindex,minlen);
+        
+        
+    }
+};
