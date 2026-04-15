4658a3f1335123666af38c2baa6a73e8c9f6764b
shrivathsasmnonavinakere <shrivathsasmnonavinakere@gmail.com>
2026-04-15 23:58:45 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+    stack<int>st;
+    stack<int>minst;
+public:
+    MinStack() {
+    }
+    void push(int val) {
+        st.push(val);
+        if(minst.empty()||val<=minst.top()){
+            minst.push(val);
+        }
+    }
+    void pop() {
+        if(st.top()==minst.top()){
+            minst.pop();
+        }
+        st.pop();   
+    }
+    int top() {
+        return st.top();   
+    }
+    int getMin() {
+        return minst.top();   
+    }
+};
