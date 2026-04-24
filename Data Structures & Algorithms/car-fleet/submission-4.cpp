c6d6629b77c90d482d713404bed0ab10d0a4c36a
shrivathsasmnonavinakere <shrivathsasmnonavinakere@gmail.com>
2026-04-24 20:04:31 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        stack<float> st;
+        map<int,float> mpp;
+
+        for(int i = 0; i < position.size(); i++){
+            float time = (float)(target - position[i]) / speed[i];
+            mpp[position[i]] = time;
+        }
+
+        int cer = 0;
+
+        for(auto i = mpp.rbegin(); i != mpp.rend(); i++){
+            if(st.empty() || i->second > st.top()){
+                cer++;
+                st.push(i->second);   
+            }
+        }
+
+        return cer; 
+    }
+};
